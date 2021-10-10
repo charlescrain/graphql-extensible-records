1bc3f9b58d1beeaa0687298f5084844b10f89c8f
Ruben Astudillo <ruben.astud@gmail.com>
2021-10-10 14:43:33 -0300

    Add a class to tell on what graphql schema to run an query

--- src/Language/GraphQL/Extensible/Class.hs
+++ src/Language/GraphQL/Extensible/Class.hs
@@ -1,7 +1,18 @@
+{-# language DeriveLift #-}
 module Language.GraphQL.Extensible.Class where
 
-import Data.Text (Text)
 import Data.Proxy (Proxy)
+import Data.Text (Text)
+import Data.Aeson.Types (ToJSON)
+import Language.Haskell.TH.Syntax (Lift(..))
 
 class GraphQLQuery args response | response -> args where
-  queryText :: Proxy response -> Text
+    queryText :: Proxy response -> Text
+
+data Schema = Admin | Standard
+    deriving (Show, Eq, Lift)
+
+--- | This tells us on what postgres/graphql schema does make sense to run
+--- | this query. Only argument have instances of this class.
+class ToJSON arg => HasSchemaInfo arg where
+    schemaInfo :: Maybe arg -> Schema
